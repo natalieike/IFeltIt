9d55f4f2a2518e0e5e0827b6d456aec80033ab8f
stanley <chw058@gmail.com>
2017-08-08 19:56:32 -0700

    cleaned up map.js

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,42 +1,13 @@
-if (navigator.geolocation) {
+/* replace the following with Jasmine's code */
 
-  console.log("geolocation support");
-
-} else {
-
-  console.log("oh no!");
-
-};
-
-var lat;
-var lng;
-
-window.onload = function() {
-  var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-    //console.log(startPos.coords.latitude);
-    //console.log(startPos.coords.longitude);
-    lat = startPos.coords.latitude;
-    lng = startPos.coords.longitude;
-
-      var latLng = new google.maps.LatLng(startPos.coords.latitude,startPos.coords.longitude);
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-      });
-
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);
-};
-
-
+//sample query URL to test getting data onto map
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-08-01&endtime=2017-08-05&limit=10";
-var idMap = $('#map').html();
-console.log(document.getElementById('map'));
-console.log(idMap);
+
+//var to hold the returned ajax
 var eqArray = [];
 
+
+//Stanley's code for google map
 var map;
       
 function initMap() {
@@ -49,15 +20,19 @@ function initMap() {
         
     });
 
-  // Create a <script> tag and set the USGS URL as the source.
- // var script = document.createElement('script');
-  
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  
-  //document.getElementsByTagName('head')[0].appendChild(script);
+  function getCircle(magnitude) {
+        return {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: 'red',
+          fillOpacity: .5,
+          scale: Math.pow(2, magnitude) * 2,
+          strokeColor: 'black',
+          strokeWeight: .5
+        };
+      }
 
+  //sample ajax to get the data from usgs appear on the map.
+  //merge the code with jasmine's version
   $.ajax({
 
     url: queryUrl,
@@ -66,7 +41,7 @@ function initMap() {
 
   }).done(function(response){
 
-    //console.log(response);
+    //the following code needs to be adopted/merged with Jasmine's to ensure the proper earthquake display
     eqArray = response.features;
     console.log(eqArray);
 
@@ -74,36 +49,17 @@ function initMap() {
     
       var coords = eqArray[i].geometry.coordinates;
       var latLng = new google.maps.LatLng(coords[1],coords[0]);
+      var magnitude = eqArray[i].properties.mag;
+
       var marker = new google.maps.Marker({
         position: latLng,
-        map: map
+        map: map,
+        icon: getCircle(magnitude),
       });
     
     };
 
-    
-
-
-  })
+  });
 
       
-  }
-
-  // Loop through the results array and place a marker for each
-  // set of coordinates.
-  /*
-  window.eqfeed_callback = function(results) {
-        
-    for (var i = 0; i < results.features.length; i++) {
-    
-      var coords = results.features[i].geometry.coordinates;
-      var latLng = new google.maps.LatLng(coords[1],coords[0]);
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-      });
-    
-    }
-  
-  }
-*/
+}
