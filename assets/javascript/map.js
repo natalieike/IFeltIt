c9210ef70f868d25f059e0d69d1d0a5b5eab13fa
stanley <chw058@gmail.com>
2017-08-10 21:24:45 -0700

    combined some code

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,7 +1,9 @@
 /* replace/merge the following with Jasmine's code */
 
 //sample query URL to test getting data onto map
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-08-01&endtime=2017-08-05&limit=10";
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&offset=1"
+
+//"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-08-01&endtime=2017-08-05&limit=10";
 
 //var to hold the returned ajax
 var eqArray = [];
@@ -21,10 +23,10 @@ function initMap() {
         
     });
 
-  function getCircle(magnitude) {
+  function getCircle(magnitude, color) {
         return {
           path: google.maps.SymbolPath.CIRCLE,
-          fillColor: '#fdd835',
+          fillColor: color,
           fillOpacity: .5,
           scale: Math.pow(2, magnitude) * 2,
           strokeColor: 'black',
@@ -32,8 +34,72 @@ function initMap() {
         };
       }
 
+      //got this ection of code from jasmine. 
+      var i,list,display;
+var currentTime=moment(new Date()).format("MM/DD/YYYY HH:mm");
+$.ajax({ 
+  url: "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&offset=1",
+  method: 'GET',
+}).done(function(response) {
+  console.log(response.features);
+  list=response.features;
+  //arr=response.features;
+  for(i=0;i<list.length;i++){
+    var t=moment(list[i].properties.time).format("MM/DD/YYYY HH:mm");
+    var duration = moment(currentTime).diff(t,'minutes');
+    console.log(t,currentTime,duration);
+    if((duration/60)<=1){
+      if(duration<2){
+      display=duration+" minute ago";
+    }
+    else{
+      display=duration+" minutes ago"
+    }
+    }
+    else if(((duration/60)/24 <= 1)){
+      display=Math.floor(duration/60);
+      if(display<2){
+        display=display+" hour ago";
+      }
+      else{
+        display=display+" hours ago"
+      }
+    }
+    else{
+      display=Math.floor((duration/60)/24);
+      if(display<2){
+        display=display+" day ago";
+      }
+      else{
+        display=display+" days ago";
+      }
+    }
+  //console.log(duration.asMinutes());
+
+    //var timeRemaining=moment.diff(list[i].properties.time)
+    $("#list-wrapper").append('<li class="collection-item avatar grey darken-4" data_info="'+(i+1)+'"><div class="circle yellow darken-1 black-text center"><span class="magnitude">'+list[i].properties.mag+'</span></div><p class="list-of-eq white-text">'+list[i].properties.title+'</p><p class="time-from-current">'+display+'</p></li>');
+  }
+
+      for (var i = 0; i < list.length; i++) {
+    
+      var coords = list[i].geometry.coordinates;
+      var latLng = new google.maps.LatLng(coords[1],coords[0]);
+      var magnitude = list[i].properties.mag;
+
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: list[i].properties.title,
+
+        icon: getCircle(magnitude, '#fdd835'),
+      });
+  
+  };
+
+
   //sample ajax to get the data from usgs appear on the map.
   //merge the code with jasmine's version
+/*
   $.ajax({
 
     url: queryUrl,
@@ -52,30 +118,47 @@ function initMap() {
       var latLng = new google.maps.LatLng(coords[1],coords[0]);
       var magnitude = eqArray[i].properties.mag;
 
-      var contentString = "test";
-
-      var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-
-
       var marker = new google.maps.Marker({
         position: latLng,
         map: map,
         title: eqArray[i].properties.title,
-        icon: getCircle(magnitude),
-      });
 
-      marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-    
+        icon: getCircle(magnitude, '#fdd835'),
+      });
+   
+/*
+    google.maps.event.addListener(marker, "click", function(){
+
+      //this.setIcon(getCircle(magnitude, 'red'));
+      map.setCenter(this.getPosition());
+
+    })
+
+    google.maps.event.addListener(marker, "mouseover", function(){
+
+      this.setIcon(getCircle(this.getIcon().scale, 'red'));
+      //map.setCenter(this.getPosition());
+
+    })
+
+
+    google.maps.event.addListener(marker, "mouseout", function(){
+
+      this.setIcon(getCircle(this.getIcon().scale, '#fdd835'));
+      //map.setCenter(this.getPosition());
+
+    })
+
     };
 
+
+  
     
 
   });
 
-      
+ */   
+
+
+});
 }
