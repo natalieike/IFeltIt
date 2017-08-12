20bcc0fb5865b9febdc93da8aa7d8e9bec3284a9
Natalie Ike <nataliesenger@dhcp-v081-105.mobile.uci.edu>
2017-08-12 11:58:51 -0700

    round coordinates because firebase key can't have .

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -43,7 +43,7 @@ function tryToPushToCorrectNode (earthquakeKey, coordinates){
 
 //Click Handler for I Felt It button
 $("#feltIt").click(function(event){
-	var eqKey = "lat" + detail.geometry.coordinates[1] + "long" + detail.geometry.coordinates[0] + "time" + detail.properties.time;
+	var eqKey = "lat" + Math.round(detail.geometry.coordinates[1]) + "long" + Math.round(detail.geometry.coordinates[0]) + "time" + detail.properties.time;
 	//var location = getGeolocation();
 	var location = [0, 0];
 	pushUserDataToDb(eqKey, location);
